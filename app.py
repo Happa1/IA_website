1496edccdb8b1c967bab1e881d32f0281847b850
Happa1 <2025.manaha.ueda@uwcisak.jp>
2024-07-27 13:48:17 +0900

    Staff Page

--- app.py
+++ app.py
@@ -130,14 +130,12 @@ def survey(appointment_id, app_date):
     print(app_date)
     print(type(app_date))
     if request.method=="POST":
-        print('here')
         patient_id = user_id
         symptom = request.form.get('symptom')
         temperature1 = request.form.get('temperature1')
         temperature2 = request.form.get('temperature2')
         temperature = float(f"{temperature1}.{temperature2}")
         note = request.form.get('note')
-        print('here')
         time= db_connection.search(query=f"""
         SELECT start_time, end_time FROM appointments WHERE id = {appointment_id}""", multiple=False)
 
@@ -151,7 +149,7 @@ def survey(appointment_id, app_date):
         SELECT id FROM record WHERE patient_id = {patient_id} AND start_time='{time[0]}'
         """
         record_id = db_connection.search(query=record_id_query, multiple=False)[0]
-        print(record_id)
+
         query_appointment=f"""
         UPDATE appointments 
         SET patient_id={patient_id}, survey_id={record_id}, date={app_date} WHERE id={appointment_id}
@@ -168,9 +166,232 @@ def survey(appointment_id, app_date):
 def appointment_check():
     return render_template('appointment_check.html')
 
-@app.route('/')
+@app.route('/staff_register',methods=['GET','POST'])
+def staff_register():
+    db_connection = DatabaseWorker("IA_database")
+    if request.method == "POST":
+        uname = request.form.get('uname')
+        password = request.form.get('psw')
+        conf_password = request.form.get('psw_conf')
+
+        valid = True
+        user_err=False
+        psw_err=False
+        psw_cnf_err = False
+
+        staff_table = db_connection.search(query="SELECT * FROM staff", multiple=True)
+
+        for row in staff_table:
+            if row[1]==uname:
+                user_err=True
+                print("the user name already exist")
+                valid=False
+                break
+
+        if len(password)<8:
+            psw_err=True
+            valid = False
+        if password!=conf_password:
+            psw_cnf_err=True
+            valid = False
+
+        hash_text=f"name{uname}, pass{password}"
+        hash=make_hash(hash_text)
+
+        if valid:
+            query = f"""INSERT INTO staff (name,signature)
+            values ('{uname}','{hash}');
+            """
+            db_connection.run_query(query=query)
+            db_connection.close()
+            return redirect(url_for('staff_login'))
+
+        db_connection.close()
+        return render_template('staff_register.html', psw_err=psw_err, psw_cnf_err=psw_cnf_err, user_err=user_err)
+
+    db_connection.close()
+    return render_template('staff_register.html')
 
 
+@app.route('/staff_login',methods=['GET','POST'])
+def staff_login():
+    db_connection = DatabaseWorker("IA_database")
+    login_err = False
+    if request.method == 'POST':
+        uname = request.form.get('uname')
+        password = request.form.get('psw')
+        results = db_connection.search(query="""SELECT * FROM staff""", multiple=True)
+        for row in results:
+            signature = row[2]  # hash text
+            hash_text = f"name{uname}, pass{password}"
+            valid = check_hash(hashed_text=signature, text=hash_text)
+            if valid:
+                user_id = row[0]
+                session['user_id'] = user_id
+                return redirect(url_for('owner_home'))
+            else:
+                login_err = True
+    db_connection.close()
+    return render_template('staff_login.html', login_err=login_err)
+
+
+@app.route('/owner_home')
+def owner_home():
+    return render_template('owner_home.html')
+
+@app.route('/appointment_view')
+def appointment_view():
+    db_connection = DatabaseWorker("IA_database")
+    query=f"""SELECT * FROM appointments
+    """
+    appointment_data=db_connection.search(query=query, multiple=True)
+
+    current_time = datetime.now().time()
+    app_start_time = time(17, 0)  # 午後5時
+    app_end_time = time(23, 59)   # 深夜12時
+    if (app_start_time <= current_time <= app_end_time):
+        app_date = (datetime.today() + timedelta(days=1)).date()
+    else:
+        app_date=datetime.today().date()
+
+    appointment_list = []
+    index=0
+    for a in appointment_data:
+        appointment_list.append(list(a))
+        if a[1] != None:
+            patient_name = db_connection.search(query=f"SELECT name FROM patients WHERE id = {a[1]}", multiple=False)[0]
+            appointment_list[index].append(patient_name)
+            index += 1
+        else:
+            appointment_list[index].append('None')
+            index += 1
+    return render_template('appointment_view.html',appointments=appointment_list, app_date=app_date)
+
+@app.route('/record_detail/<int:patient_id>/<app_date>', methods=['GET','POST'])
+def record_detail(patient_id, app_date):
+    db_connection = DatabaseWorker("IA_database")
+    query=f"""
+    SELECT * FROM record WHERE date ='{app_date}' AND patient_id={patient_id}"""
+    app_detail=db_connection.search(query=query, multiple=False)
+    patient_data=db_connection.search(query=f"""
+    SELECT * FROM patients WHERE id={patient_id}""",multiple=False)
+    return render_template('record_detail.html', app_detail=app_detail, patient=patient_data)
+
+@app.route('/patient_search',methods=['GET','POST'])
+def patient_search():
+    db_connection = DatabaseWorker("IA_database")
+
+    if request.method == "POST":
+        id = request.form.get('search_id')
+        print(id)
+        date = request.form.get('search_date')
+        print(date)
+        no_record = False
+        if id=="" and date=="":
+            print('enter something')
+            no_record=True
+        elif id!="" and date=="":
+            records=db_connection.search(f"""
+            SELECT * FROM record WHERE patient_id={int(id)}""", multiple=True)
+            if not records:
+                no_record=True
+                print(f"No records found for id: {id}")
+        elif id=="" and date!="":
+            records = db_connection.search(f"""
+                        SELECT * FROM record WHERE date='{date}'""", multiple=True)
+            if not records:
+                no_record=True
+                print(f"No records found for date: {date}")
+        else:
+            records = db_connection.search(f"""
+                                    SELECT * FROM record WHERE patient_id={int(id)} AND date='{date}'""", multiple=True)
+            if not records:
+                no_record=True
+                print(f"No records")
+
+        if no_record == False:
+            record_list=[]
+            for r in records:
+                r=list(r)
+                print(r)
+                patient_id= str(r[1]).zfill(6)
+                print(patient_id)
+                patient_name=db_connection.search(query=f"""
+                SELECT name FROM patients WHERE id={r[0]}""",multiple=False)[0]
+                r.append(patient_id) #id 10
+                r.append(patient_name) #id 11
+                record_list.append(r)
+            print(record_list)
+        return render_template('patient_search.html', no_record=no_record, records=record_list)
+
+    return render_template('patient_search.html')
+
+
+@app.route('/patient_detail/<int:patient_id>')
+def patient_detail(patient_id):
+    db_connection = DatabaseWorker("IA_database")
+    patient_data = db_connection.search(query=f"""
+        SELECT * FROM patients WHERE id={patient_id}""", multiple=False)
+    patient_record = db_connection.search(query=f"""
+    SELECT * FROM record WHERE patient_id = {patient_id}""",multiple=True)
+    print(patient_record)
+    return render_template('patient_detail.html',patient_data=patient_data, patient_record=patient_record)
+
+@app.route('/owner_news')
+def owner_news():
+    db_connection = DatabaseWorker("IA_database")
+    news = db_connection.search(query=f"""
+    SELECT * FROM news""", multiple=True)
+    return render_template('owner_news.html', news=news)
+
+@app.route('/news_create',methods=['GET','POST'])
+def news_create():
+    db_connection = DatabaseWorker("IA_database")
+    if request.method == "POST":
+        title = request.form.get('title')
+        content = request.form.get('content')
+        type = request.form.get('type')
+        date = datetime.now().strftime('%Y-%m-%d')
+
+        query=f"""
+        INSERT INTO news ('date','title','content','type')
+        values ('{date}','{title}','{content}','{type}')"""
+
+        db_connection.run_query(query=query)
+        db_connection.close()
+        return redirect(url_for('owner_news'))
+
+    return render_template('news_create.html')
+
+@app.route('/news_edit/<int:news_id>',methods=['GET','POST'])
+def news_edit(news_id):
+    db_connection = DatabaseWorker("IA_database")
+    news_data=db_connection.search(query= f"""
+    SELECT * FROM news where id={news_id}""", multiple=False)
+    if request.method == "POST":
+        new_title = request.form.get('title')
+        new_content = request.form.get('content')
+        new_type = request.form.get('type')
+        query=f"""
+        UPDATE news SET title='{new_title}',content='{new_content}',type='{new_type}' WHERE id = {news_id}"""
+        db_connection.run_query(query=query)
+        db_connection.close()
+        return redirect(url_for('owner_news'))
+    db_connection.close()
+    return render_template('news_edit.html', news=news_data)
+
+@app.route('/news_delete/<int:news_id>',methods=['GET','POST'])
+def news_delete(news_id):
+    db_connection = DatabaseWorker("IA_database")
+    query=f"""
+    DELETE FROM news where id={news_id}"""
+    db_connection.run_query(query=query)
+    db_connection.close()
+    return redirect(url_for('owner_news'))
+
+@app.route('/pre_password')
+def pre_password():
+    return render_template('pre_password_edit.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
